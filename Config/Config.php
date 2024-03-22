a8ecb7d9ac9762571b4ebf20fb1ffad7c52b4bb6
SergioCasas14 <scasas@roi-up.es>
2024-03-22 09:05:34 +0100

    parte 2 del tfg

---
+++ Config/Config.php
@@ -0,0 +1,11 @@
+<?php
+const BASE_URL = "http://localhost/tienda-virtual/";
+const HOST = "localhost";
+const USER = "root";
+const PASS = "";
+const DB = "tienda_virtual";
+const CHARSET = "charset=utf8";
+const TITLE = "Tienda virtual de Sergio";
+const MONEDA = "USD";
+const CLIENT_ID = "";
+?>
