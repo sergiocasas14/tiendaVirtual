259e901a5bb7d289a96a9fa5a52181636ad91925
SergioCasas14 <scasas@roi-up.es>
2024-06-02 21:01:41 +0200

    comentarios2

--- Views/index.php
+++ Views/index.php
@@ -90,6 +90,7 @@
             </div>
         </div>
         <div class="row">
+             
             <div class="col-12 col-md-4 p-5 mt-3">
                 <a href="#"><img src="./assets/img/category_img_01.jpg" class="rounded-circle img-fluid border"></a>
                 <h5 class="text-center mt-3 mb-3">Relojes</h5>
